f193524196c1b9e622faf7a926e4ba9ac3d0904f
Davi Marangoni <davi.marangoni@hotmail.com>
2021-02-20 23:20:02 -0300

    erros e execoes

---
+++ Erros/execoes.php
@@ -0,0 +1,19 @@
+<?php
+
+error_reporting(E_ALL);
+
+$divisor = 10;
+
+
+
+try {
+    if($divisor == 0){
+        throw new Exception("Nao e possivel dividir por 0, defina outro valor", 1);
+    }
+
+    $valor = 100 / $divisor;
+    echo $valor;
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+}
+
---
+++ Erros/tipos.php
@@ -0,0 +1,26 @@
+<?php
+
+error_reporting(E_ALL);
+
+//exemplo NOTICE
+// $valor1 = 10;
+// echo $valor;
+
+
+//WARNING
+//$numero = NAO_EXISTE;
+
+//fatal
+// function somar($valor1, $valor2)
+// {
+//     return $valor2 + $valor1;
+// }
+// function somar($valor1, $valor2)
+// {
+//     return $valor2 + $valor1;
+// }
+
+// parse
+// echo "erro"
+
+echo "chegou ao fim";
